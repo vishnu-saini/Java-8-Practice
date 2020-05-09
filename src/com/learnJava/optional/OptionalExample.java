75b87b1e52f25015f88a8977306f503d0cd0e972
Vishnu Saini <vishnu.saini@publicissapient.com>
2020-05-09 16:28:40 +0530

    udemy practice

---
+++ src/com/learnJava/optional/OptionalExample.java
@@ -0,0 +1,42 @@
+package com.learnJava.optional;
+
+import java.util.Optional;
+
+import com.learnJava.data.Student;
+import com.learnJava.data.StudentDataBase;
+
+public class OptionalExample {
+
+	public static String getStudentName() {
+
+		// Student student = StudentDataBase.studentSupplier.get();
+		Student student = null;
+		if (student != null) {
+			return student.getName();
+		} else {
+			return null;
+		}
+	}
+
+	public static Optional<String> getStudentNameOptional() {
+
+		Optional<Student> student = Optional.ofNullable(StudentDataBase.studentSupplier.get());
+		// Optional<Student> student = Optional.ofNullable(null);
+
+		if (student.isPresent()) {
+			return student.map(Student::getName); // don't get confused this with streams map method. Both are
+													// different.
+		}
+		return Optional.empty();
+	}
+
+	public static void main(String[] args) {
+
+		if (getStudentNameOptional().isPresent()) {
+			System.out.println(getStudentNameOptional().get());
+		} else {
+			System.out.println("Name is returned as empty.");
+		}
+
+	}
+}
